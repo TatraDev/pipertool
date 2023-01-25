e77c3aa5c56ea78e5d5e28bb74e0307792179cca
sokolegg <sokolegg@yandex.ru>
2023-01-26 02:23:23 +0400

    fastapi refactoring

--- piper/base/executors/fastapi.py
+++ piper/base/executors/fastapi.py
@@ -7,102 +7,18 @@ from piper.envs import get_env, is_current_env, is_docker_env, Env
 from piper.utils import docker_utils
 from piper.utils.logger_utils import logger
 from piper.base.executors import HTTPExecutor
+from piper.base.executors.utils import write_requirements, copy_piper, copy_scripts, build_image, add_row, add_packages_to_install
 
 import asyncio
 import inspect
 import sys
 import time
-from abc import ABC, abstractmethod
-from typing import Dict
-from distutils.dir_util import copy_tree
-import subprocess
 
-import aiohttp
 import docker
 import requests
 from pydantic import BaseModel  # , BytesObject, ListOfStringsObject
 
 cfg = get_configuration()
-
-
-def add_packages_to_install(packages_list):
-    row = f'RUN apt install -y {" ".join(packages_list)} \n'
-    return row
-
-
-def add_row(row):
-    return f'{row} \n'
-
-
-def copy_piper(path: str):
-    cfg = get_configuration()
-    copy_tree(cfg.piper_path, f"{path}piper")
-
-
-def copy_scripts(path: str, scripts: Dict[str, str]):
-    for script_name, script_path in scripts.items():
-        with open(f"{path}/{script_name}.py", "w") as output:
-            with open(script_path, "r") as current_file:
-                output.write(current_file.read())
-
-
-def write_requirements(path, requirements):
-    with open(f"{path}/requirements.txt", "w") as output:
-        output.write("\n".join(requirements))
-
-
-def _build_image_old(path: str, docker_image):
-    """
-    deprecated:
-    this doesn't stream logs from docker build
-    TODO: fix it in build_image_old and use docker-py
-    """
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    image = docker_image.render()
-
-    with open(f"{path}Dockerfile", "w") as output:
-        output.write(image)
-
-    streamer = client.build(path=path,
-                            decode=True,
-                            tag=docker_image.tag,
-                            quiet=False,
-                            buildargs={"progress": "plain"},
-                            timeout=cfg.docker_build_timeout)
-    for chunk in streamer:
-        logger.info(chunk)
-
-    logger.info(f'piper built image {docker_image.tag}')
-
-
-def build_image(path: str, docker_image):
-    """
-    Build docker image
-    OLD build_image_old doesn't stream logs from docker build
-    TODO: fix it in build_image_old and use docker-py instead of Popen
-    """
-    image = docker_image.render()
-
-    with open(f"{path}Dockerfile", "w") as output:
-        output.write(image)
-    cmd = ['docker', 'build',  "--progress", "plain", '-t', docker_image.tag, path]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    with process.stdout:
-        for line in iter(process.stdout.readline, b''):
-            logger.info(line)
-
-    logger.info(f'piper built image {docker_image.tag}')
-
-
-def run_container(image: str, ports: Dict[int, int]):
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    container = client.containers.run(image, detach=True, ports=ports)
-    for log in container.logs():
-        logger.info(f'executor run_container: {log}')
-    logger.info(f'container is {container}')
-    time.sleep(10)
-
-    return container
 
 
 def wait_for_fast_api_app_start(host, external_port, wait_on_iter, n_iters):
